2997a5ad801725fed9b6d6f694a45cc13566176d
gwesseling <gerardwesseling16@gmail.com>
2020-09-13 19:31:55 +0200

    Feat: initial commit

---
+++ Code/MyArrayList/MyArrayList.cs
@@ -0,0 +1,89 @@
+﻿namespace AD
+{
+    public partial class MyArrayList : IMyArrayList
+    {
+        private int[] data;
+        private int size;
+
+        public MyArrayList(int capacity)
+        {
+            data = new int[capacity];
+            size = 0;
+        }
+
+        public void Add(int n)
+        {
+            if (size >= data.Length) {
+                throw new MyArrayListFullException();
+            }
+
+            data[size] = n;
+            size++;
+        }
+
+        public int Get(int index)
+        {
+            if (index < 0 || size == 0|| index > (size - 1)) {
+                throw new MyArrayListIndexOutOfRangeException();
+            }
+
+            return data[index];
+        }
+
+        public void Set(int index, int n)
+        {
+            if (index < 0 || index > (size - 1)) {
+                throw new MyArrayListIndexOutOfRangeException();
+            }
+
+            data[index] = n;
+        }
+
+        public int Capacity()
+        {
+            return data.Length;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Clear()
+        {
+            data = new int[data.GetLength(0)];
+            size = 0;
+        }
+
+        public int CountOccurences(int n)
+        {
+            int occurences = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                if (data[i] == n) {
+                    occurences++;
+                }
+            }
+
+            return occurences;
+        }
+
+        public override string ToString() {
+            string result = "NIL";
+
+            if (size > 0) {
+                result = "[";
+                for (int i = 0; i < size; i++) {
+                    result += data[i];
+
+                    if (i < size - 1) {
+                        result += ",";
+                    }
+                }
+                result += "]";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Code/MyLinkedList/MyLinkedList.cs
@@ -0,0 +1,116 @@
+﻿namespace AD
+{
+    public partial class MyLinkedList<T> : IMyLinkedList<T>
+    {
+        public MyLinkedListNode<T> head;
+        private int size;
+
+        public MyLinkedList()
+        {
+            this.size = 0;
+        }
+
+        public void AddFirst(T data)
+        {
+            MyLinkedListNode<T> node = new MyLinkedListNode<T>() {
+                data = data
+            };
+
+            if (size > 0) {
+                node.next = head;
+            }
+
+            this.head = node;
+            this.size++;
+        }
+
+        public T GetFirst()
+        {
+            if (size == 0) {
+                throw new MyLinkedListEmptyException();
+            }
+
+            return this.head.data;
+        }
+
+        public void RemoveFirst()
+        {
+
+            if (size == 0) {
+                throw new MyLinkedListEmptyException();
+            }
+
+            MyLinkedListNode<T> node = head.next;
+            head = node;
+            size--;
+        }
+
+        public int Size()
+        {
+            return this.size;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            size = 0;
+        }
+
+        public void Insert(int index, T data)
+        {
+            if (index < 0 || index > size) {
+                throw new MyLinkedListIndexOutOfRangeException();
+            }
+
+            MyLinkedListNode<T> prevNode = null;
+            MyLinkedListNode<T> node = head;
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T>() {
+                data = data,
+                next = null,
+            };
+
+            for (int i = 0; i < index + 1; i++) {
+                if (i == index) {
+                    if (i == 0) {
+                        this.head = newNode;
+                    }
+
+                    if (prevNode != null) {
+                        newNode.next = prevNode.next;
+                        prevNode.next = newNode;
+                    }
+                    size++;
+                }
+
+                if (node != null) {
+                    prevNode = node;
+                    node = node.next;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "NIL";
+
+            if (size > 0) {
+                result = "[";
+
+                MyLinkedListNode<T> node = head;
+                for (int i = 0; i < size; i++) {
+                    result += node.data;
+
+                    if (i < size - 1) {
+                        result += ",";
+                    }
+
+                    node = node.next;
+                }
+
+                result += "]";
+            }
+
+            return result;
+        }
+    }
+}
