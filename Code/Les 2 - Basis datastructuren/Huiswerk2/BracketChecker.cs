0a160cfc5cc5926afe2f9e59f54655f6fe5262aa
gwesseling <gerardwesseling16@gmail.com>
2020-09-15 16:45:03 +0200

    Feat: bracketchecker1

--- Code/Les 2 - Basis datastructuren/Huiswerk2/BracketChecker.cs
+++ Code/Les 2 - Basis datastructuren/Huiswerk2/BracketChecker.cs
@@ -5,9 +5,29 @@ namespace AD
     public static class BracketChecker
     {
 
+        static MyStack<char> stack = new MyStack<char>();
+
         public static bool CheckBrackets(string s)
         {
-            throw new System.NotImplementedException();
+            try {
+                foreach (char c in s) {
+                    if (c.Equals('(')) {
+                        stack.Push(c);
+                    } else if (!stack.IsEmpty()) {
+                        stack.Pop();
+                    } else {
+                        return false;
+                    }
+                }
+
+                if (stack.IsEmpty()) {
+                    return true;
+                }
+
+                return false;
+            } catch {
+                return false;
+            }
         }
 
         public static bool CheckBrackets2(string s)
