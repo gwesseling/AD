539066c78334d24052ea5b9fc1d16e3ea05f7f96
gwesseling <gerardwesseling16@gmail.com>
2020-09-22 12:20:36 +0200

    feat: homework week 3 (1,2,3,4)

---
+++ Code/Les 3 - Recursie en sorteren/Tests/TestUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AD
+{
+    public class TestUtils
+    {
+        public static string TrimmedStringWithSingleSpaces(string s)
+        {
+            return Regex.Replace(s, @"\s+", " ").Trim();
+        }
+
+    }
+}
